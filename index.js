19cc2357c857ff0372469f3c00b1f84459a28c53
Nathan Hoang <sliqq123@gmail.com>
2023-10-03 15:08:31 -0700

    added inquirer questions and prompt

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color keyword or a hexadecimal number for the text:',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color keyword or a hexadecimal number for the shape:',
+    }
+];
+
+// inquirer.prompt(questions).then(data => {
